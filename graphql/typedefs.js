9738346214ade642b9f973d8872b8f0826c7e049
Fabian Dean <fabian@fabiandean.dev>
2020-10-13 16:13:27 -0700

    Stripped auth contents for different approach to the app

--- graphql/typedefs.js
+++ graphql/typedefs.js
@@ -1,11 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    enum CollectionType {
-        PLUGIN
-        THEME
-        MIXED
-    }
     type Ratings {
         one: Int
         two: Int
@@ -64,66 +59,11 @@ const typeDefs = gql`
         screenshot_url: String
         preview_url: String
     }
-    type User {
-        _id: String!
-        username: String!
-        email: String!
-        password: String
-        facebook_id: String
-        premium: Boolean
-        collection_ids: [ID]
-        date_created: String
-    }
-    input UserInput {
-        username: String!
-        email: String!
-        password: String!
-    }
-    type AuthPayload {
-        user: User
-    }
-    type Collection {
-        _id: String!
-        name: String!
-        owner_id: ID!
-        date_created: String
-        type: CollectionType
-        plugins: [Plugin]
-        themes: [Theme]
-    }
-    input CollectionInput {
-        name: String!
-        type: CollectionType
-    }
     type Query {
-        currentUser: User
-        getUser(email: String!): User
-        getCollection(collection_id: ID!): Collection
-        getCollections(user_id: ID!): [Collection]
         searchPlugin(query: String!): [Plugin]
         searchTheme(query: String!): [Theme]
         getPluginInfo(slug: String!): Plugin
         getThemeInfo(slug: String!): Theme
-    }
-    type Mutation {
-        login(email: String!, password: String!): AuthPayload
-        signup(
-            username: String!
-            email: String!
-            password: String!
-        ): AuthPayload
-        logout: Boolean
-        createCollection(
-            email: String!
-            collection: CollectionInput!
-        ): Collection!
-        updateUser(user_id: ID!, user: UserInput!): User!
-        updateCollection(
-            collection_id: ID!
-            collection: CollectionInput!
-        ): Collection!
-        deleteUser(email: String!): Boolean!
-        deleteCollection(collection_id: ID!): Boolean!
     }
 `;
 
--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { DateTime } = require('luxon');
-const session = require('express-session');
-const passport = require('passport');
-const FacebookStrategy = require('passport-facebook');
-const { GraphQLLocalStrategy, buildContext } = require('graphql-passport');
-const { v4: uuid } = require('uuid');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const typeDefs = require('./graphql/typedefs');
 const resolvers = require('./graphql/resolvers');
-const User = require('./models/User/User');
 
 const init = async () => {
     const PORT = process.env.PORT || 4000;
@@ -24,128 +15,9 @@ const init = async () => {
     const corsConfig = {
         methods: ['GET', 'POST', 'PUT', 'PATCH'],
         origin: process.env.CLIENT_URL,
-        credentials: true,
     };
 
     app.use(cors(corsConfig));
-
-    /**
-     * Configure express-session with uuid
-     */
-    app.use(
-        session({
-            genid: (req) => uuid(),
-            secret: process.env.SESSION_SECRET,
-            resave: false,
-            saveUninitialized: false,
-        }),
-    );
-
-    /**
-     * Configure Mongoose and connect to db
-     */
-
-    await mongoose
-        .connect(process.env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-        })
-        .then(() => {
-            console.log('✅ Successfully connected to database.');
-        })
-        .catch((err) => {
-            console.error('❌ Error connecting to database.\n', err);
-        });
-
-    /**
-     * Configure Passport
-     */
-    app.use(passport.initialize());
-    app.use(passport.session());
-
-    passport.use(
-        new GraphQLLocalStrategy(async (email, password, done) => {
-            const users = await User.schema.statics.getUsers();
-            const matchingUser = await User.schema.methods.matchingUser(
-                email,
-                password,
-            );
-            const error = matchingUser
-                ? null
-                : new Error('Username/password is incorrect');
-            done(error, matchingUser);
-        }),
-    );
-
-    const facebookOptions = {
-        clientID: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        callbackURL: 'http://localhost:4000/auth/facebook/callback',
-        profileFields: ['id', 'email', 'first_name', 'last_name'],
-    };
-
-    const facebookCallback = async (
-        accessToken,
-        refreshToken,
-        profile,
-        done,
-    ) => {
-        const users = await User.schema.statics.getUsers();
-        const matchingUser = await users.find(
-            (user) => user.facebook_id === profile.id,
-        );
-
-        if (matchingUser) {
-            done(null, matchingUser);
-            return;
-        }
-
-        const newUserConfig = {
-            _id: uuid(),
-            username: `${profile.name.givenName} ${profile.name.familyName}`,
-            email:
-                profile.emails && profile.emails[0] && profile.emails[0].value,
-            password: null,
-            facebook_id: profile.id,
-        };
-
-        const newUser = await User.create({
-            ...newUserConfig,
-            premium: false,
-            collection_ids: [],
-            date_created: DateTime.utc().toISODate(),
-        });
-
-        done(null, newUser);
-    };
-
-    passport.use(new FacebookStrategy(facebookOptions, facebookCallback));
-
-    passport.serializeUser((user, done) => {
-        done(null, user._id);
-    });
-
-    passport.deserializeUser(async (id, done) => {
-        const users = await User.schema.statics.getUsers();
-        const matchingUser = await users.find((user) => {
-            return user._id === id;
-        });
-        done(null, matchingUser);
-    });
-
-    app.get(
-        '/auth/facebook',
-        passport.authenticate('facebook', { scope: ['email'] }),
-    );
-    app.get(
-        '/auth/facebook/callback',
-        passport.authenticate('facebook', {
-            successRedirect: 'http://localhost:4000/graphql',
-            failureRedirect: 'http://localhost:4000/graphql',
-        }),
-    );
 
     /**
      * Configure Apollo Server
@@ -153,20 +25,6 @@ const init = async () => {
     const server = new ApolloServer({
         typeDefs,
         resolvers,
-        context: ({ req, res }) => {
-            return buildContext({
-                req,
-                res,
-                User,
-                getUser: () => req.user,
-                logout: () => req.logout(),
-            });
-        },
-        playground: {
-            settings: {
-                'request.credentials': 'include',
-            },
-        },
     });
 
     server.applyMiddleware({ app });
